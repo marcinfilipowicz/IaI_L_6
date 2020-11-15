2377ae10cd5013b3b87b55e1774ca35a8bcf0be8
marcinfilipowicz <73066958+marcinfilipowicz@users.noreply.github.com>
2020-11-15 17:42:10 +0100

    Dodanie scrolla

--- js/script.js
+++ js/script.js
@@ -1,5 +1,4 @@
 let button = document.querySelector('#bg');
 button.addEventListener('click', ()=> {
     let body = document.getElementsByTagName('body')[0];
-    body.style.backgroundColor = '#' + Math.floor(Math.random()*16777215).toString(16);
-});
+    body.style.backgroundColor = '#' + Math.floor(Math.random()*16777215).toString(16);});
